a08e2cb891570e8999f93b09734c06c50c0d76e2
Wole Solana <osolana@thoughtworks.com>
2016-04-25 15:03:05 -0400

    Create project with dependencies added
    Authors: jhenders@thoughtworks.com, osolana@thoughtworks.com

---
+++ DemoAppCarthage/DemoAppCarthage/Hello.swift
@@ -0,0 +1,15 @@
+//
+//  Calculator.swift
+//  DemoAppCarthage
+//
+//  Created by Wole Solana on 2016/04/22.
+//  Copyright © 2016 ThoughtWorks. All rights reserved.
+//
+
+import Foundation
+
+class Hello {
+  func hi() -> String {
+    return "Hello World"
+  }
+}
